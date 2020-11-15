40479dc50060ab76a0f1f962ed0559fe552a6e2d
Avinash M <59330319+Avinash7714@users.noreply.github.com>
2020-11-15 11:44:39 +0530

    Update signUp.js

--- screens/signUp.js
+++ screens/signUp.js
@@ -93,7 +93,7 @@ export default function SignUp({ navigation }) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "whitesmoke",
+        backgroundColor: "white",
         alignItems: "center",
         justifyContent: "center",
     },
